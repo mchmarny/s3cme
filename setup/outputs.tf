4be89224389cc22e794e007c1e80f1aa9409559f
Mark Chmarny <mark@chmarny.com>
2023-01-14 04:48:03 -0800

    test on tag

--- setup/outputs.tf
+++ setup/outputs.tf
@@ -1,26 +1,31 @@
 # Description: Outputs for the deployment
 
-output "PROJECT_ID" {
-  value       = data.google_project.project.name
-  description = "Project ID to use in Auth action for GCP in GitHub."
+output "REG_URI" {
+  value       = "${google_artifact_registry_repository.registry.location}-docker.pkg.dev"
+  description = "Artifact Registry location."
 }
 
-output "CI_SERVICE_ACCOUNT" {
+output "IMAGE_URI" {
+  value       = "${data.google_project.project.name}/${google_artifact_registry_repository.registry.name}"
+  description = "Artifact Registry location."
+}
+
+output "IMAGE_NAME" {
+  value       = google_artifact_registry_repository.registry.name
+  description = "Artifact Registry location."
+}
+
+output "SA_EMAIL" {
   value       = google_service_account.github_actions_user.email
   description = "Service account to use in GitHub Action for federated auth."
 }
 
-output "IDENTITY_PROVIDER" {
+output "PROVIDER_ID" {
   value       = google_iam_workload_identity_pool_provider.github_provider.name
   description = "Provider ID to use in Auth action for GCP in GitHub."
 }
 
-output "REGISTRY_URI" {
-  value       = "${google_artifact_registry_repository.registry.location}-docker.pkg.dev/${data.google_project.project.name}/${google_artifact_registry_repository.registry.name}"
-  description = "Artifact Registry location."
-}
-
 output "KMS_KEY" {
-  value       = data.google_kms_crypto_key_version.version.name
+  value       = "gcpkms://${data.google_kms_crypto_key_version.version.name}"
   description = "Cosign-formated URI to the signing key."
 }
