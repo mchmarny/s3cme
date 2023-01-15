240a355548e92e762732d660763fee07155687a2
Mark Chmarny <mark@chmarny.com>
2023-01-15 06:06:43 -0800

    full test

--- setup/outputs.tf
+++ setup/outputs.tf
@@ -2,20 +2,25 @@
 
 output "REG_URI" {
   value       = "${google_artifact_registry_repository.registry.location}-docker.pkg.dev/${data.google_project.project.name}/${google_artifact_registry_repository.registry.name}"
-  description = "Fully qualified Artifact Registry URI."
+  description = "Fully qualified Artifact Registry URI to use in Auth Actions."
+}
+
+output "IMG_NAME" {
+  value       = google_artifact_registry_repository.registry.name
+  description = "Image name to use in Auth Actions."
 }
 
 output "SA_EMAIL" {
   value       = google_service_account.github_actions_user.email
-  description = "Service account to use in GitHub Action for federated auth."
+  description = "Service account to use in GitHub Actions."
 }
 
 output "PROVIDER_ID" {
   value       = google_iam_workload_identity_pool_provider.github_provider.name
-  description = "Provider ID to use in Auth action for GCP in GitHub."
+  description = "Provider ID to use in Auth Actions."
 }
 
 output "KMS_KEY" {
   value       = "gcpkms://${data.google_kms_crypto_key_version.version.name}"
-  description = "Cosign-formated URI to the signing key."
+  description = "Cosign-formated KMS key path to use in Auth Actions."
 }
